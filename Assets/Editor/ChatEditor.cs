0c228415b15cdac94c3e04e6942964ff63115fc0
Egor <egorzhuchkov2013@yanex.ru>
2021-04-24 16:39:39 +0300

    Chat editor fixes

--- Assets/Editor/ChatEditor.cs
+++ Assets/Editor/ChatEditor.cs
@@ -6,19 +6,36 @@ namespace Editor
   public class ChatEditor : UnityEditor.Editor
   {
     private const string ChatOwnerLabel = "Chat owner";
-    private int _choiceIndex;
+    private const string ChatOwnerIndex = "ChatOwnerIndex";
+    private static int _choiceIndex;
     private readonly string[] _defaultOptions = {"None"};
- 
+
+    private void OnEnable()
+    {
+      _choiceIndex = EditorPrefs.GetInt(ChatOwnerIndex);
+    }
+
+    private void OnDisable()
+    {
+      EditorPrefs.SetInt(ChatOwnerIndex, _choiceIndex);
+    }
+
     public override void OnInspectorGUI ()
     {
       DrawDefaultInspector();
       
       if (target is Chat chat && chat.Members.Count > 0)
       {
-        if (chat.Members.Count < _choiceIndex)
+        var previousOwner = chat.Owner;
+        
+        if (_choiceIndex >= chat.Members.Count)
           _choiceIndex = 0;
 
         _choiceIndex = EditorGUILayout.Popup(ChatOwnerLabel, _choiceIndex, chat.Members.ToArray());
+
+        if (previousOwner == chat.Members[_choiceIndex])
+          return;
+        
         chat.Owner = chat.Members[_choiceIndex];
         EditorUtility.SetDirty(target);
       }
